8fade8bed73169f3048ee393826b0a57c9c7e42f
kareemtaha3 <tahakareem27@gmail.com>
2024-10-16 13:58:01 +0300

    update

--- obj/Debug/net8.0/RealStats.AssemblyInfo.cs
+++ obj/Debug/net8.0/RealStats.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealStats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19371078708ff3e287e9f6c542285c51c9f0672c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fed09a735cfee9303158420ae8e84572fa2b85d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealStats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealStats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
